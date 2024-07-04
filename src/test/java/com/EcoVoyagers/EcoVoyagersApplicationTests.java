e25ef23f772b8444d6f38d7fda33ae6cc8434db8
miangels1978 <einsteinvie@gmail.com>
2024-07-04 16:45:33 +0200

    Actualizacion

---
+++ src/test/java/com/EcoVoyagers/EcoVoyagersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EcoVoyagers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoVoyagersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
